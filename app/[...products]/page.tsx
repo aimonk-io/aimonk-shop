fe2e416a26c69470d78f64283412cab7a56adc11
rohit133 <rohityou000@gmail.com>
2025-01-04 12:57:21 +0530

    updates

---
+++ app/[...products]/page.tsx
@@ -0,0 +1,13 @@
+
+
+const ProductPage = () => {
+    return (
+        <div>
+            <h1>{"Product Page"}</h1>
+            <p>{"Hi This is  jsut a sample product Page."}</p>
+            <p>Price: ${}</p>
+        </div>
+    );
+};
+
+export default ProductPage;
--- libs/Types/MobileNavbar/Index.ts
+++ libs/Types/MobileNavbar/Index.ts
@@ -1,2 +1,2 @@
 export * from "@/libs/Types/MobileNavbar/MenuItem/MenuItem";
-export * from "@/libs/Types/MobileNavbar/MobileMenuProps/MobileMenuProps"
+export * from "@/libs/Types/MobileNavbar/MobileMenuProps/MobileMenuProps";
